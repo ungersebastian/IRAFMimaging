ab0938a9afed261e7725b8de5a227c250fc229eb
Dr. Daniela Täuber <31131708+TauDan@users.noreply.github.com>
2020-04-29 18:47:17 +0200

    Update PiFMhyPIRana.py
    - Corrected the split wavenumber to 1644
    - Created a function for PCA
    - Applied evaluation of mean and PCA separately to the lower laser tuner range and the higher laser tuner range parts of the spectra

--- IRAFspec/PiFMhyPIRana.py
+++ IRAFspec/PiFMhyPIRana.py
@@ -5,7 +5,7 @@ Created on Fri Apr 24 08:06:26 2020
 
 @author: ungersebastian
 
-Modified on Thu Apr 28 by Daniela Taeuber
+(Last) Modified on Thu Apr 29 by Daniela Taeuber
 """
 
 #%% imports & parameters
@@ -25,7 +25,7 @@ path_final = join(path_dir, path_import)
 
 #%% functions
 
-def mean_spectrum(spc_norm, my_data):
+def mean_spectrum(spc_norm, my_wl, my_data):
     '''
     Calculates and plots mean spectra with standard deviations
     '''
@@ -34,15 +34,67 @@ def mean_spectrum(spc_norm, my_data):
 
     my_fig = plt.figure()
     ax = plt.subplot(111)
-    ax.fill_between(x = my_data['wavelength'], y1 = mean_spc+std_spc, y2 = mean_spc-std_spc, alpha = 0.6)
+    ax.fill_between(x = my_wl, y1 = mean_spc+std_spc, y2 = mean_spc-std_spc, alpha = 0.6)
     plt.gca().invert_xaxis() #inverts values of x-axis
-    ax.plot(my_data['wavelength'], mean_spc)
+    ax.plot(my_wl, mean_spc)
     ax.set_xlabel('wavenumber ['+my_data['PhysUnitWavelengths']+']')
-    ax.set_ylabel('intensity (normalized)')
-    ax.set_yticklabels([])
+    ax.set_ylabel('PiFM [V]') #+my_data['files'=6,'PhysUnit']+'
+    #ax.set_yticklabels([])
     plt.title('mean spectrum')
     my_fig.tight_layout()
     return mean_spc
+
+def export_spectrum(spectrum):
+    '''
+    Exports (saves) spectral data in ascii(?) text(?) format  - planned
+    '''
+  
+def PCA_spectrum(my_spectra, my_wl, my_data, my_coord):
+    '''
+    Calculates and plots two principal components for a set of spectral data
+    '''
+    from sklearn.decomposition import PCA
+    ncomp = 2
+    model = PCA(n_components=ncomp)
+
+    transformed_data = model.fit(my_spectra).transform(my_spectra).T
+    loadings = model.components_
+
+    my_fig = plt.figure()
+    ax = plt.subplot(111)
+    plt.gca().invert_xaxis() #inverts values of x-axis
+    for icomp in range(ncomp):
+        ax.plot(my_wl, loadings[icomp], label='PC'+str(icomp+1) )
+    ax.set_xlabel('wavenumber ['+my_data['PhysUnitWavelengths']+']')
+    ax.set_ylabel('PiFM [V]')
+    #ax.set_yticklabels([])
+    ax.legend()
+    plt.title('PCA-Loadings (principal components)')
+
+    my_fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.plot(transformed_data[0], transformed_data[1], '.')
+    ax.set_xlim(np.quantile(transformed_data[0], 0.05),np.quantile(transformed_data[0], 0.95))
+    ax.set_ylim(np.quantile(transformed_data[1], 0.05),np.quantile(transformed_data[1], 0.95))
+    ax.set_xlabel('PC1')
+    ax.set_ylabel('PC2')
+    plt.title('scatterplot')
+    my_fig.tight_layout()
+
+    maps = [zeros.copy() for icomp in range(ncomp)]
+    for icomp in range(ncomp):
+        maps[icomp][my_coord] = transformed_data[icomp]
+        maps[icomp] = np.reshape(maps[icomp], (my_data['xPixel'], my_data['yPixel']) )
+    
+        my_fig = plt.figure()
+        ax = plt.subplot(111)
+        plt.colorbar( ax.imshow(maps[icomp], cmap = 'coolwarm', extent = my_data.extent()) )
+    
+        ax.set_xlabel('x scan ['+my_data['XPhysUnit']+']')
+        ax.set_ylabel('y scan ['+my_data['YPhysUnit']+']')
+        plt.title('factors PC'+str(icomp+1))    
+
+        my_fig.tight_layout() 
 
 #%% loads data and plots associated VistaScan parameter images
 
@@ -55,7 +107,10 @@ my_data.plot_all()
 my_spc = my_data.return_spc()
 my_wl  = my_data['wavelength']
 
-split_wl = 1648
+split_wl = 1644
+
+my_wl_low = my_wl[my_wl <= split_wl]
+my_wl_high = my_wl[my_wl > split_wl]
 
 spc_low = my_spc[:,my_wl <= split_wl]
 spc_high = my_spc[:,my_wl > split_wl]
@@ -70,6 +125,8 @@ select = my_sum_low * my_sum_high != 0
 coord = np.arange(len(spc_low))
 zeros = np.zeros(len(spc_low))
 
+#%% Evaluates spectra from spatial positions where both laser tuners recorded and plots mean spectra and PCA
+
 coord = coord[select]
 spc_low_s = spc_low[select]
 spc_high_s = spc_high[select]
@@ -83,54 +140,49 @@ spc_train = np.concatenate([spc_low_s, spc_high_s], axis = 1)
 my_sum = np.sum(spc_train, axis = 1)
 spc_norm = np.array([spc/s for spc, s in zip(spc_train, my_sum)])
 
-#%%
 # PlotIt
-mean_spc = mean_spectrum(spc_norm, my_data)
+mean_spc = mean_spectrum(spc_train, my_data['wavelength'], my_data)
+PCA_spectrum(spc_train, my_data['wavelength'], my_data, coord) #why is this already showing normalized plots?
+
+mean_spc = mean_spectrum(spc_norm, my_data['wavelength'], my_data)
+#spc_norm-mean_spc; substracting the mean_spc does not make any difference
+PCA_spectrum(spc_norm, my_data['wavelength'], my_data, coord)
 
 
-#%%
+#%% Evaluates spectra from spatial positions where the lower spectral range tuners recorded and plots mean and PCS
+coord_low = np.arange(len(spc_low))
+zeros = np.zeros(len(spc_low))
+coord_low = coord_low[select_low]
+spc_low_s = spc_low[select_low]
+my_sum_low_s = my_sum_low[select_low]
 
-from sklearn.decomposition import PCA
-ncomp = 2
-model = PCA(n_components=ncomp)
 
-transformed_data = model.fit(spc_norm-mean_spc).transform(spc_norm-mean_spc).T
-loadings = model.components_
+spc_low_n = np.array([spc/s for spc, s in zip(spc_low_s, my_sum_low_s)])
+my_sum = np.sum(spc_low_n, axis = 1)
+spc_norm = np.array([spc/s for spc, s in zip(spc_low_s, my_sum)])
 
-my_fig = plt.figure()
-ax = plt.subplot(111)
-plt.gca().invert_xaxis() #inverts values of x-axis
-for icomp in range(ncomp):
-    ax.plot(my_data['wavelength'], loadings[icomp], label='PC'+str(icomp+1) )
-ax.set_xlabel('wavenumber ['+my_data['PhysUnitWavelengths']+']')
-ax.set_ylabel('intensity (normalized)')
-ax.set_yticklabels([])
-ax.legend()
-plt.title('PCA-Loadings')
+# PlotIt
+mean_spc = mean_spectrum(spc_low_s, my_wl_low, my_data)
+PCA_spectrum(spc_low_s, my_wl_low, my_data, coord_low)
 
-my_fig = plt.figure()
-ax = plt.subplot(111)
-ax.plot(transformed_data[0], transformed_data[1], '.')
-ax.set_xlim(np.quantile(transformed_data[0], 0.05),np.quantile(transformed_data[0], 0.95))
-ax.set_ylim(np.quantile(transformed_data[1], 0.05),np.quantile(transformed_data[1], 0.95))
-ax.set_xlabel('PC1')
-ax.set_ylabel('PC2')
-plt.title('scatterplot')
-my_fig.tight_layout()
+mean_spc = mean_spectrum(spc_low_n, my_wl_low, my_data)
+PCA_spectrum(spc_low_n, my_wl_low, my_data, coord_low)
 
-maps = [zeros.copy() for icomp in range(ncomp)]
-for icomp in range(ncomp):
-    maps[icomp][coord] = transformed_data[icomp]
-    maps[icomp] = np.reshape(maps[icomp], (my_data['xPixel'], my_data['yPixel']) )
-    
-    my_fig = plt.figure()
-    ax = plt.subplot(111)
-    plt.colorbar( ax.imshow(maps[icomp], cmap = 'coolwarm', extent = my_data.extent()) )
-    
-    ax.set_xlabel('x scan ['+my_data['XPhysUnit']+']')
-    ax.set_ylabel('y scan ['+my_data['YPhysUnit']+']')
-    plt.title('factors PC'+str(icomp+1))
-    
+#%% Evaluates spectra from spatial positions where the higher spectral range tuners recorded and plots mean and PCS
+coord_high = np.arange(len(spc_high))
+zeros = np.zeros(len(spc_high))
+coord_high = coord_high[select_high]
+spc_high_s = spc_high[select_high]
+my_sum_high_s = my_sum_high[select_high]
 
-    my_fig.tight_layout()
 
+spc_high_n = np.array([spc/s for spc, s in zip(spc_high_s, my_sum_high_s)])
+my_sum = np.sum(spc_high_n, axis = 1)
+spc_norm = np.array([spc/s for spc, s in zip(spc_high_s, my_sum)])
+
+# PlotIt
+mean_spc = mean_spectrum(spc_high_s, my_wl_high, my_data)
+PCA_spectrum(spc_high_s, my_wl_high, my_data, coord_high)
+
+mean_spc = mean_spectrum(spc_high_n, my_wl_high, my_data)
+PCA_spectrum(spc_high_n, my_wl_high, my_data, coord_high)
