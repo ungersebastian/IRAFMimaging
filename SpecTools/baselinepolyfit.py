bd4cd8bee3a8ce18662b9d27f5e68e7cb5a23811
Mohammad Soltaninezhad <sinawpppp@gmail.com>
2021-04-20 21:32:40 +0000

    Replace baselinepolyfit.py

--- SpecTools/baselinepolyfit.py
+++ SpecTools/baselinepolyfit.py
@@ -1,220 +1,156 @@
-'''baseline correction
-Mohammad Soltaninezhad sinawpppp@gmail.com
-Dr.Daniela Täuber
- 23 FEB 2021 '''
-'''program help:
-raw spectra plot After run ,then you asked if you want to do whole spectrum fitting or region select mode?
-1.you can set whole spc fit poly,program ask for polynomial degree for each stage 
-2.if you select region select mode you have 3 choice!!!
-2.1By input==1 you  can select a desire regions by set upper and lower wavelength,
-!2.2By input==2 you are able to divide spc into two different regions,
-and fit every region with ideal poly order,
-2.3By input==3 program divide spc into 3 different regions,each can be fitted by desire poly order
- '''
-#%%
-#Imports and plot of original data
 import peakutils
 from peakutils.plot import plot as pplot
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#call the file
-def find_index(wavelenght_arr,number):
-    for index,value in enumerate(wavelenght_arr):
-        if abs(value-number)<0.0000001:
-            return index
-    return index
-#recive data
-#PATH="D:/SpecTools/RetavforAnaconda.csv" #csv or txt format
-PATH="D:/SpecTools/AuPMMA_60nm_FTIRspecularReflection_H20.txt" #csv or txt format
-df=pd.read_csv(PATH, sep="\t", skiprows=[0] , delimiter="\t") #in skiprows 0 refers to one text line, \t = delimited by tab
-#df=pd.read_csv(PATH, sep="\t", skiprows=[0,1,2,3,4] , delimiter=",") #use delimiter =for import file and sep for export file
-df.to_numpy() #set export file format
-data=df.values.T
-print("data",data.shape)
-y=data
-x=data[0] #wavelenght
-x = np.nan_to_num(x)
-y = np.nan_to_num(y)
-y = np.delete(y, 0, 0 )
-r=np.zeros(df.shape)
-print("df",df.shape)
-plt.plot(x,y.T)
-plt.title('raw spectral')
+from scipy import sparse
+import math
+from scipy.sparse.linalg import spsolve
+
+PATH = "PDha_1.csv"  # csv or txt format
+df = pd.read_csv( PATH, sep="\t", skiprows=[0, 1, 2, 3, 4],delimiter="," )  # in skiprows write the rows which contain text in order to elliminate them
+df.to_numpy()
+class baselinecorrection:
+    def __init__(self,path):
+        self.path=path
+        self.read_data()
+    def read_data(self):
+        df = pd.read_csv( self.path, sep="\t", skiprows=[0, 1, 2, 3, 4],
+                          delimiter="," )  # in skiprows write the rows which contain text in order to eliminate them
+        df.to_numpy()
+        data = df.values.T
+        y = data
+        x = data[0]  # wavelength
+        self.x = np.nan_to_num( x )
+        y = np.nan_to_num( y )
+        self.y = np.delete( y, 0, 0 ) #spectral data
+
+
+    def baseline_subtraction(self,rawspc):
+        b = len( rawspc )
+        t = int( input( "plz enter polynomial degree " ) )
+        c = np.zeros( rawspc.shape )
+        for i in range( b ):
+            base = peakutils.baseline(rawspc[i], t)  # choose order of polynomial here
+            c[i] = rawspc[i] - base
+            c[c < 0] = 0
+        return c, base
+
+    def baseline_als(self,rawspc, lam, p, niter):
+        """
+
+        :param lam:  λ for smoothness default 10**5
+        :param p:  p for asymmetry default: 0.1
+        :param niter: number of iteration default: 10
+        :return: least square baseline error
+        """
+        y = rawspc[0, :]
+        l = len( y )
+        eye = np.eye( l )
+        D = sparse.csc_matrix( np.diff( eye, 2 ) )
+        w = np.ones( l )
+        for i in range( niter ):
+            W = sparse.spdiags( w, 0, l, l )
+            Z = W + lam * D.dot( D.transpose() )
+            z = spsolve( Z, w * y )
+            w = p * (y > z) + (1 - p) * (y < z)
+        return(z)
+    def find_index(self, wavelenght_arr, number):
+        for index, value in enumerate(wavelenght_arr):
+            if abs(value - number) < 0.0000001:
+                index = float(index)
+                return index
+        return index
+
+    def region_select(self):
+        """
+
+        :return: new x and y due to selected region
+        """
+        wu = float( input( "plz enter upper boundry wavelenght " ) )
+        ou = self.find_index(self.x, wu )
+        wl = float( input( "plz enter lower boundry wavelenght " ) )
+        ol = self.find_index(self.x, wl )
+        ou=int(ou)
+        ol=int(ol)
+        xi = self.x[ol:ou, ]
+        yi = self.y[:, ol:ou]
+        return xi, yi
+    def plot_sub(self,raw,wavelebgth):
+        fig, ax = plt.subplots(3)
+        ax[0].plot(wavelebgth, raw.T)
+        ax[0].set_title( "Raw spectra", fontsize=15 )
+        ax[1].plot(wavelebgth, c.T)
+        ax[1].set_title( "corrected spectra", fontsize=15 )
+        ax[2].plot(wavelebgth, base)
+        ax[2].set_title( "noise", fontsize=15 )
+        plt.savefig( 'FTIR subtraction poly.png' )
+        plt.tight_layout()
+        plt.show()
+        plt.plot(wavelebgth, c.T)
+        plt.title("corrected graph")
+        plt.savefig('corrected graph.png')
+        plt.show()
+        return
+    def plot_als(self,raw,wavelebgth):
+        c = self.baseline_als(raw,lam=10**5, p=0.1, niter=10)
+        fig, ax = plt.subplots(3)
+        ax[0].plot(wavelebgth, raw.T)
+        ax[0].set_title("Raw spectra", fontsize=15)
+        ax[1].plot(wavelebgth, (raw - c).T)
+        ax[1].set_title("corrected spectra", fontsize=15)
+        ax[2].plot(wavelebgth, c)
+        ax[2].set_title("noise", fontsize=15)
+        plt.savefig('corrected graph.png')
+        plt.tight_layout()
+        plt.show()
+        plt.plot(wavelebgth, (raw - c).T)
+        plt.title("FTIR corrected")
+        plt.savefig('FTIR corrected.png')
+        plt.show()
+        return
+
+
+
+
+c1 = baselinecorrection(PATH)
+wl = c1.x
+raw = c1.y
+plt.plot(wl, raw.T)
+plt.title("raw spectra")
 plt.show()
-#%%
-#fitting
-g=int(np.size(y, 1))
-l=input("Do you need to select desire regions? yes =y no =n ").lower()
-if l == "n": #1. whole spectrum fitting
-    b = len(y)
-    t = int( input( "plz enter polynomial degree " ) )
-    c = np.zeros(y.shape)
-    for i in range(b):
-        base = peakutils.baseline(y[i], t)  # choose order of polynomial here
-        c[i] = y[i] - base
-        c[c < 0] = 0
-    r[:,0]=x.transpose()
-    r[:,1:]=c.transpose()
-    np.savetxt('baseline corrected spectra.txt', r,delimiter='\t') #change plot title based on your poly degree
-    fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-    fig.suptitle( f'FTIR poly :{t}' )
-    ax1.plot( x, y.T )
-    ax2.plot( x, c.T )
-    ax3.plot( x, base )
-    plt.title( "noise" )
-#plt.savefig( 'FTIR poly.png' )
-    plt.show()
-    plt.plot(x,c.T)
-    plt.title( "FTIR corrected" )
-#plt.savefig( 'FTIR corrected.png' )
-    plt.show()
-elif l == "y": #2.region select mode
-    f=int(input("How many regions do you need(between 2-3)enter 1 for specific region"))
-    if f==1:  #2.1. desire region
-        wu=float(input("plz enter upper boundry wavelenght "))
-        ou = find_index(x, wu)
-        wl=float(input("plz enter lower boundry wavelenght "))
-        t=int(input("plz enter polynomial degree "))
-        ol = find_index(x, wl)
-        xi=x[ol:ou,]
-        yi=y[:,ol:ou]
-        b = len( yi )
-        ci = np.zeros( yi.shape )
-        for i in range( b ):
-            basei = peakutils.baseline( yi[i], t )  # choose order of polynomial here
-            ci[i] = yi[i] - basei
-            ci[ci < 0] = 0
-        np.savetxt( 'baseline corrected spectra.txt', ci ) #change plot title base on your poly degree
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'FTIR poly{t}')
-        ax1.plot( xi, yi.T )
-        ax2.plot( xi, ci.T )
-        ax3.plot( xi, basei )
-        plt.title("noise")
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xi, ci.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-
-    elif f==2: #2.2.two different region
-        f1=float(input("choose boundry wavelenght"))
-        o=find_index(x,f1)
-        y1 = y[:,:o ]
-        y2= y[:, o:]
-        xd = x[:o,]
-        xu = x[o:,]
-        b = len( y )
-        t1 = int( input( "plz enter polynomial degree for first region " ) )
-        t2 = int( input( "plz enter polynomial degree for second region " ) )
-        c1 = np.zeros( y1.shape )
-        c2 = np.zeros( y2.shape )
-        for i in range( b ):
-            base1 = peakutils.baseline( y1[i], t1 ) #select first region poly order
-            base2 = peakutils.baseline( y2[i], t2 )  #select second region poly order
-            c1[i] = y1[i] - base1
-            c2[i] = y2[i] - base2
-            c1[c1 < 0] = 0
-            c1[c1 < 0] = 0
-        np.savetxt( 'baseline corrected spectra1.txt', c1 )
-        np.savetxt( 'baseline corrected spectra2.txt', c2 )
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'First region poly{t1}' ) #change title base on your poly degree
-        ax1.plot( xd, y1.T )
-        ax2.plot( xd, c1.T )
-        ax3.plot( xd, base1 )
-        plt.title( "noise" )
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xd, c1.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'second region poly{t2}' )
-        ax1.plot( xu, y2.T )
-        ax2.plot( xu, c2.T )
-        ax3.plot( xu, base2 )
-        plt.title( "noise" )
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xu, c2.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-    elif f==3: #2.3.three different regions
-        fh = float(input("choose upper boundry wavelenght"))
-        fl = float(input("choose lower boundry wavelenght"))
-        oh = int(find_index(x,fh))
-        ol = int(find_index(x,fl))
-        yl = y[:, 0:ol]
-        ym = y[:, ol:oh]
-        yh = y[:, oh:]
-        xd = x[:ol, ]
-        xm=x[ol:oh,]
-        xu = x[oh:, ]
-        t1 = int( input( "plz enter first region polynomial degree " ) )
-        t2 = int( input( "plz enter second regionpolynomial degree " ) )
-        t3 = int( input( "plz enter third region polynomial degree " ) )
-        b = len( y )
-        cl = np.zeros( yl.shape )
-        cm = np.zeros( ym.shape )
-        ch = np.zeros( yh.shape )
-        for i in range( b ):
-            basel = peakutils.baseline(yl[i], 3)
-            basem = peakutils.baseline(ym[i], 3)
-            baseh = peakutils.baseline(yh[i], 3)
-            cl[i] = yl[i] - basel
-            cm[i] = ym[i] - basem
-            ch[i] = yh[i] - baseh
-            cl[cl < 0] = 0
-            cm[cm < 0] = 0
-            ch[ch < 0] = 0
-        np.savetxt( 'baseline corrected spectra1.txt', cl )
-        np.savetxt( 'baseline corrected spectra2.txt', cm )
-        np.savetxt( 'baseline corrected spectra3.txt', ch )
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'First region poly{t1}' )
-        ax1.plot( xd, yl.T )
-        ax2.plot( xd, cl.T )
-        ax3.plot( xd, basel )
-        plt.title( "noise" )
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xd, cl.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'second region poly{t2}' )
-        ax1.plot( xm, ym.T )
-        ax2.plot( xm, cm.T )
-        ax3.plot( xm, basem )
-        plt.title( "noise" )
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xm, cm.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-
-        fig, (ax1, ax2, ax3) = plt.subplots( 3 )
-        fig.suptitle( f'third region poly{t3}' )
-        ax1.plot( xu, yh.T )
-        ax2.plot( xu, ch.T )
-        ax3.plot( xu, baseh )
-        plt.title( "noise" )
-        plt.savefig( 'FTIR poly.png' )
-        plt.show()
-        plt.plot( xu, ch.T )
-        plt.title( "FTIR corrected" )
-        plt.savefig( 'FTIR corrected.png' )
-        plt.show()
-#%% Transpose and Export corrected data
-#Transpose results data set
-#corrected=c.values.T
+r=np.zeros(df.shape)
 
 
+l = input( "Do you need to select desire regions? yes =y no =n" ).lower()
+if l == "n":  # 1. whole spectrum fitting
+    l1 = input( "least square or subtraction method? least square =a subtraction=s" ).lower()
+    if l1 == "s":
+        c, base = c1.baseline_subtraction( raw )
+        r[:, 0] = wl.transpose()
+        r[:, 1:] = c.transpose()
+        np.savetxt( 'baseline corrected spectra.txt', r, delimiter='\t' )  # change plot title base on your poly degree
+        c1.plot_sub(raw,wl)
+    elif l1 == "a":
+        G = c1.baseline_als( lam=10 ** 5, p=0.1, niter=10 )
+        G1 = raw - G
+        r[:, 0] = wl.transpose()
+        r[:, 1:] = G1.transpose()
+        np.savetxt( 'baseline corrected spectra.txt', r, delimiter='\t' )  # change plot title base on your poly degree
+        c1.plot_als(raw,wl)
+    else:
+        raise ValueError( "this value is not valid for arg method(method must be s or a)" )
+elif l == "y":
+    l1 = input( "least square or subtraction method? least square =a subtraction=s" ).lower()
+    if l1 == "s":
+        wli, rawi = c1.region_select()
+        c, base = c1.baseline_subtraction( rawi )
+        c1.plot_sub(rawi,wli)
+    elif l1 == "a":
+        wli, rawi = c1.region_select()
+        c = c1.baseline_als(rawi,lam=10**5,p=0.1,niter=10)
+        c1.plot_als(rawi,wli)
+    else:
+        raise ValueError("this value is not valid for arg method(method must be s or a)")
+else:
+    raise ValueError("this value is not valid for arg method(method must be y or n)")
