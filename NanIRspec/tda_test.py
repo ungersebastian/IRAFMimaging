20dcd6b404ec54ce2de137074bd8861abd3f83eb
Sebastian Unger <basti.unger@googlemail.com>
2021-09-24 09:53:47 +0200

    Testfiles tda
    Signed-off-by: Sebastian Unger <basti.unger@googlemail.com>

--- NanIRspec/tda_test.py
+++ NanIRspec/tda_test.py
@@ -27,6 +27,7 @@ dim = (dim, dim)
 data_norm = np.array([d/n if n > 0 else d*0 for d,n in zip(data, l2)])
 data_id = np.arange(n_data)
 
+
 #%%
 
 from sklearn.neighbors import NearestNeighbors
@@ -37,11 +38,11 @@ import matplotlib as mpl
 n_neighbors = 10
 nbrs = NearestNeighbors(n_neighbors=n_neighbors+1, algorithm='ball_tree').fit(data_norm)
 distances, indices = nbrs.kneighbors(data_norm)
+weights= 1/(distances+np.mean(distances)*(1E-5))
 
 G = nx.Graph()
 G.add_nodes_from(data_id)
-
-[[G.add_edge(node, ind, length = edge_len) for ind, edge_len in zip(node_ind, node_edge)] for node, node_ind, node_edge in zip(data_id,indices,distances)]
+[[G.add_edge(node, ind, length = edge_len) for ind, edge_len in zip(node_ind, node_edge)] for node, node_ind, node_edge in zip(data_id,indices,weights)]
 
 pos = nx.spring_layout(G, weight='length')
 
@@ -49,6 +50,48 @@ plt.figure()
 nx.draw(G, pos, node_size = 1)
 plt.show()
 
+#%%
+nSubsets = 40
+
+lensSubset = np.zeros(n_data)-1
+pValArray = np.array(list(pos.values()))
+pPosArray = np.array(list(pos.keys()))
+minX, maxX = np.amin(pValArray[:,0]), np.amax(pValArray[:,0])
+minY, maxY = np.amin(pValArray[:,1]), np.amax(pValArray[:,1])
+
+vX = np.std(pValArray[:,0])
+vY = np.std(pValArray[:,1])
+dX = (maxX - minX)
+dY = (maxY - minY)
+
+xy = (vX/dX)/(vY/dY)
+
+ny = np.sqrt(nSubsets/xy)
+nx = np.ceil(ny*xy).astype(int)
+ny = np.ceil(ny).astype(int)
+
+dX = dX/nx
+dY = dY/ny
+
+xList = [[minX+nEdge*dX, minX+(nEdge+1)*dX] for nEdge in range(nx)]
+yList = [[minY+nEdge*dY, minY+(nEdge+1)*dY] for nEdge in range(ny)]
+
+for kx in range(nx):
+    for ky in range(ny): 
+        sel = (pValArray[:,0] >= xList[kx][0]) * (pValArray[:,0] <= xList[kx][1]) * (pValArray[:,1] >= yList[ky][0]) * (pValArray[:,1] <= yList[ky][1])
+        lensSubset[pPosArray[sel]] = kx*ny+ky
+        
+lens_img = lensSubset.reshape(dim)
+
+cmap = plt.cm.jet 
+cmaplist = [cmap(i) for i in range(cmap.N)]
+cmaplist[0] = (.5, .5, .5, 1.0)
+cmap = mpl.colors.LinearSegmentedColormap.from_list(
+    'Custom cmap', cmaplist, cmap.N)
+
+plt.figure()
+plt.imshow(lens_img, cmap=cmap)
+plt.show()        
 #%%
 
 # non overlapping subsets
